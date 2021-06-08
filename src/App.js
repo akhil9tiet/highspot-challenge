ba7db61e98f06e43d956d8a50a288b8e91f7fc3a
Akhil <akhil9tiet@gmail.com>
2021-06-08 01:30:40 -0700

    feat: get all data from endpoint

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
+const fetchData = async () => {
+	let response = await fetch(`https://api.elderscrollslegends.io/v1/cards`);
+	return response.json();
+};
+
 function App() {
+	let [data, setData] = useState([]);
+
+	useEffect(() => {
+		fetchData().then((res) => setData(res.cards));
+	}, []);
+
+	console.log(data);
 	return (
 		<div className='App'>
-			<p>Yesy</p>
+			<p>Test</p>
 		</div>
 	);
 }
