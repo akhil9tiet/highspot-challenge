d516ae9237026f038754f781925cd213e4026a54
Akhil <akhil9tiet@gmail.com>
2021-06-08 01:47:54 -0700

    fix: paginated response

--- src/App.js
+++ src/App.js
@@ -1,22 +1,26 @@
 import React, { useState, useEffect } from 'react';
 import './App.css';
 
-const fetchData = async () => {
-	let response = await fetch(`https://api.elderscrollslegends.io/v1/cards`);
+const pageSize = 20;
+
+const fetchData = async (page) => {
+	let response = await fetch(`https://api.elderscrollslegends.io/v1/cards/?pageSize=${pageSize}&page=${page}`);
 	return response.json();
 };
 
 function App() {
 	let [data, setData] = useState([]);
+	let [page, setPage] = useState(1);
 
 	useEffect(() => {
-		fetchData().then((res) => setData(res.cards));
-	}, []);
+		fetchData(page).then((res) => setData(res.cards));
+	}, [page]);
 
 	console.log(data);
 	return (
 		<div className='App'>
 			<p>Test</p>
+			<button onClick={() => setPage(page + 1)}>Click</button>
 		</div>
 	);
 }
