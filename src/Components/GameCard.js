a6a1ffee06d9932dbfd8e83c90912d9d2b751719
Akhil <akhil9tiet@gmail.com>
2021-06-09 01:37:42 -0700

    feat: image centered, card footer side by side

--- src/Components/GameCard.js
+++ src/Components/GameCard.js
@@ -7,20 +7,25 @@ import Typography from '@material-ui/core/Typography';
 
 export default function GameCard({ id, imageUrl, name, text, setName, type }) {
 	return (
-		<Grid className='item' item xs={12} sm={6} md={4} lg={3} key={id}>
+		<Grid className='item' item xs={12} sm={4} md={4} lg={3} key={id}>
 			<Card className='card'>
 				<CardContent className='card-content'>
 					<CardMedia style={{ height: 500, width: 300 }} image={imageUrl} title={id} />
 					<div className='content'>
-						<Typography variant='body2' component='h1'>
+						<Typography gutterBottom variant='h4' component='h1'>
 							{name}
 						</Typography>
-						<p>{name}</p>
-						<p>{setName}</p>
-						<p>{type}</p>
-						<br />
-						<div className='icon'>
-							<p>{text}</p>
+						<Typography variant='body2' color='textSecondary' component='p'>
+							{text}
+						</Typography>
+
+						<div className='card-footer'>
+							<Typography variant='body2' color='textPrimary' component='p'>
+								{setName}
+							</Typography>
+							<Typography variant='body2' component='p'>
+								{type}
+							</Typography>
 						</div>
 					</div>
 				</CardContent>
